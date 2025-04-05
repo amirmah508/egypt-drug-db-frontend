82f822d1f1947da5884fddcd6b07bfff14183d37
Amir Hanafi <amirmah@gmail.com>
2025-04-04 22:26:08 -0400

    Add missing src/index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/globals.css"; // or delete this line if no styles yet
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
